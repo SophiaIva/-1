9f668037cf37e4d4cea1d4a53608dcafba52010d
Sophia_Iva <sophia.999zzzzzz@gmail.com>
2026-03-11 21:08:27 +0300

    Update task 3.2.py

--- task 3.2.py
+++ task 3.2.py
@@ -1,12 +1,12 @@
 # TODO Напишите функцию find_common_participants
-def find_common_participants(firstg, secondg, r = ','):
-    f = set(firstg.split(r))
-    s = secondg.split(r)
-    return list(f.intersection(s))
+def find_common_participants(first_group, second_group, separator=','):
+    first_set = set(first_group.split(separator))
+    second_set = set(second_group.split(separator))
+    return list(first_set.intersection(second_set))
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-temp = find_common_participants(participants_first_group, participants_second_group, '|')
-print(temp)
+common_participants = find_common_participants(participants_first_group, participants_second_group, '|')
+print(common_participants)
